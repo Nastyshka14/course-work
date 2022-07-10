cf47a7ad105c58db30cded80721bfc066ab69d2d
Nastyshka14 <nastya.korshun.1997@mail.ru>
2022-07-10 14:25:14 +0400

    resolve coflicts

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,42 +1,43 @@
-import React, { useCallback, useContext, useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { AuthContext } from "../context/auth.context";
-import { Button, Dropdown, Menu, Input, Space, suffix } from "antd";
-import "./Navbar.css";
+import React, { useCallback, useContext, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../context/auth.context';
+import { Button, Dropdown, Menu, Input, Space, suffix } from 'antd';
+import './Navbar.css';
 import {
   UserOutlined,
   DatabaseOutlined,
   LogoutOutlined,
   UsergroupDeleteOutlined,
   AudioOutlined,
-  LockFilled
-  AudioOutlined, 
-  PoweroffOutlined
-} from "@ant-design/icons";
-import { useHttp } from "../hooks/http.hook";
+  LockFilled,
+  PoweroffOutlined,
+} from '@ant-design/icons';
+import { useHttp } from '../hooks/http.hook';
 const { Search } = Input;
 const emojiesList = [
   {
-    "title": "100",
-    "symbol": "💯",
-    "keywords": "hundred points symbol symbol wow wow win win perfect perfect parties parties"
+    title: '100',
+    symbol: '💯',
+    keywords:
+      'hundred points symbol symbol wow wow win win perfect perfect parties parties',
   },
   {
-    "title": "1234",
-    "symbol": "🔢",
-    "keywords": "input symbol for numbers symbol"
+    title: '1234',
+    symbol: '🔢',
+    keywords: 'input symbol for numbers symbol',
   },
   {
-    "title": "Grinning",
-    "symbol": "😀",
-    "keywords": "grinning face happy smiley emotion emotion"
-  }]
+    title: 'Grinning',
+    symbol: '😀',
+    keywords: 'grinning face happy smiley emotion emotion',
+  },
+];
 
-export const Navbar = ({toggleTheme}) => {
+export const Navbar = ({ toggleTheme }) => {
   const auth = useContext(AuthContext);
   const isAuthenticated = auth.isAuthenticated;
   const username = auth.username;
-  const userRole = auth.userRole
+  const userRole = auth.userRole;
   const { request } = useHttp();
   const { token } = useContext(AuthContext);
   const [collections, setCollections] = useState([]);
@@ -60,8 +61,9 @@ export const Navbar = ({toggleTheme}) => {
     if (inputValue) {
       setEmojies([
         ...collections.filter((emoji) =>
-        Object.values(emoji)
-        .toLocaleLowerCase().includes(inputValue.toLocaleLowerCase())
+          Object.values(emoji)
+            .toLocaleLowerCase()
+            .includes(inputValue.toLocaleLowerCase())
         ),
       ]);
     } else {
@@ -75,95 +77,102 @@ export const Navbar = ({toggleTheme}) => {
 
   const menuItemsUser = [
     {
-      key: "1",
+      key: '1',
       label: <Link to="/collections">Мои коллекции</Link>,
       icon: <DatabaseOutlined />,
     },
     {
-      key: "3",
+      key: '3',
       label: <div onClick={logoutHandler}>Выйти</div>,
       icon: <LogoutOutlined />,
       danger: true,
     },
-  ]
+  ];
 
   const menuItemsAdmin = [
     {
-      key: "1",
+      key: '1',
       label: <Link to="/collections">Мои коллекции</Link>,
       icon: <DatabaseOutlined />,
     },
     {
-      key: "2",
+      key: '2',
       label: <Link to="/users">Users List</Link>,
       icon: <UsergroupDeleteOutlined />,
     },
     {
-      key: "3",
+      key: '3',
       label: <div onClick={logoutHandler}>Выйти</div>,
       icon: <LogoutOutlined />,
       danger: true,
     },
-  ]
+  ];
 
-  const menu = userRole === 'Admin' ? <Menu items={ menuItemsAdmin } /> : <Menu items={ menuItemsUser } />
+  const menu =
+    userRole === 'Admin' ? (
+      <Menu items={menuItemsAdmin} />
+    ) : (
+      <Menu items={menuItemsUser} />
+    );
 
   const onSearch = (value) => searchEmoji(value);
 
-const lol = () => collections.map((item, index) => (item))
+  const lol = () => collections.map((item, index) => item);
 
-const suffix = (
-  <AudioOutlined
-    style={{
-      fontSize: 16,
-      color: '#1890ff',
-    }}
-  />
-);
+  const suffix = (
+    <AudioOutlined
+      style={{
+        fontSize: 16,
+        color: '#1890ff',
+      }}
+    />
+  );
 
   return (
     <div className="header">
       <Link className="logo" to="/homepage" />
+
       <Space direction="vertical">
-      <Search
-      placeholder="input search text"
-      allowClear
-      onSearch={onSearch}
-      style={{
-        width: 200,
-      }}
+        <Search
+          placeholder="input search text"
+          allowClear
+          onSearch={onSearch}
+          style={{
+            width: 200,
+          }}
+        />
+        <Button className="list" onClick={console.log(collections)}>
+          Войти
+        </Button>
 
-    />
-    <Button className="list" onClick={console.log(collections)} >Войти</Button>
-      
         {emojies.map((item, index) => {
-
-return <div className="list" key={index}> {item.name} {item.description} {item.theme}</div>;
-})}
-    </Space>
-          return <div styles={{backgroundColor: 'red', width: 50}} key={index}  >{symbol} {title}</div>;
+          return (
+            <div className="list" key={index}>
+              {' '}
+              {item.name} {item.description} {item.theme}
+            </div>
+          );
         })}
-    </Space>
+      </Space>
 
-    <div className="menu-wrapp">
-      {isAuthenticated ? (
-        <Dropdown overlay={menu}>
-          <div className="username">
-            <div>{username}</div>
-            <UserOutlined />
-          </div>
-        </Dropdown>
-      ) : (
-        <Link to='/'>
-          <Button type="primary">Войти</Button>
-          
-        </Link>
-      )}
+      <div className="menu-wrapp">
+        {isAuthenticated ? (
+          <Dropdown overlay={menu}>
+            <div className="username">
+              <div>{username}</div>
+              <UserOutlined />
+            </div>
+          </Dropdown>
+        ) : (
+          <Link to="/">
+            <Button type="primary">Войти</Button>
+          </Link>
+        )}
 
-      <Button ghost shape="circle" onClick={toggleTheme}><PoweroffOutlined /></Button>
-    </div>
-
-
+        <Button ghost shape="circle" onClick={toggleTheme}>
+          <PoweroffOutlined />
+        </Button>
+      </div>
     </div>
   );
 };
