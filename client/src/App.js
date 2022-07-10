7ada638e4de2246f98e9360b5f035077a5f0f2b0
Siarhei Chura <Siarhei.Chura@connectwise.com>
2022-07-10 15:31:31 +0400

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -4,37 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { useAuth } from "./hooks/auth.hook";
 import { AuthContext } from "./context/auth.context";
 import { Navbar } from "./components/Navbar";
-import {Spin} from "antd";
-import {useHttp} from "./hooks/http.hook";
-
-const stylesheets = {
-  light: "https://cdnjs.cloudflare.com/ajax/libs/antd/4.9.4/antd.min.css",
-  dark: "https://cdnjs.cloudflare.com/ajax/libs/antd/4.9.4/antd.dark.min.css"
-}
-
-const createStylesheetLink = () => {
-  const link = document.createElement('link')
-  link.rel = 'stylesheet'
-  link.id = 'antd-stylesheet'
-  document.head.appendChild(link)
-  return link
-}
-
-const getStylesheetLink = () => document.head.querySelector("#antd-stylesheet") || createStylesheetLink()
-
-const systemTheme = () =>
-  window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
-    ? 'dark'
-    : 'light'
-
-const setTheme = (theme) => {
-  localStorage.setItem('theme', theme)
-  getStylesheetLink().href = stylesheets[theme]
-}
-
-const getTheme = () => (localStorage.getItem('theme')) || systemTheme()
-
-const toggleTheme = () => setTheme(getTheme() === 'dark' ? 'light' : 'dark')
+import {toggleTheme, getTheme, setTheme} from "./shared/setThemeHelper";
 
 function App() {
   const { login, logout, token, userId, username, userRole } = useAuth();
@@ -44,10 +14,10 @@ function App() {
 
   return (
     <AuthContext.Provider value={{ login, logout, token, userId, username, userRole, isAuthenticated }}>
-        <BrowserRouter>
-          <Navbar toggleTheme={toggleTheme} />
-          <div className="container">{routes}</div>
-        </BrowserRouter>
+      <BrowserRouter>
+        <Navbar toggleTheme={toggleTheme} />
+        <div className="container">{routes}</div>
+      </BrowserRouter>
     </AuthContext.Provider>
   );
 }
